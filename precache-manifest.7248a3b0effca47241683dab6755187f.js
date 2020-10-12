18406e041ba1d760cb45320db1c74465c74c5184
Waleed A. Afifi <waleedafifich@gmail.com>
2020-10-12 16:57:12 +0300

    Updates

---
+++ precache-manifest.7248a3b0effca47241683dab6755187f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c124df424d6d082b8738d8ffa0f48e83",
+    "url": "/waleedafifi-401-advanced-javascript/resty/index.html"
+  },
+  {
+    "revision": "55a519cb6723db5b5caf",
+    "url": "/waleedafifi-401-advanced-javascript/resty/static/css/main.59c81a48.chunk.css"
+  },
+  {
+    "revision": "4d3cde4a6b0703a88176",
+    "url": "/waleedafifi-401-advanced-javascript/resty/static/js/2.ad73c84a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/waleedafifi-401-advanced-javascript/resty/static/js/2.ad73c84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a519cb6723db5b5caf",
+    "url": "/waleedafifi-401-advanced-javascript/resty/static/js/main.ba3bf93f.chunk.js"
+  },
+  {
+    "revision": "6cc781105f3d99eaa185",
+    "url": "/waleedafifi-401-advanced-javascript/resty/static/js/runtime-main.e24b35fb.js"
+  }
+]);
